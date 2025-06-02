79a3eeb542f3741adbf2c8f890a11dc3fb374b9d
ankhcaesar <ankhcaesar@gmail.com>
2025-06-02 10:49:26 -0300

    speedinsight de vencel

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react' // ✅ importa el componente
 import './index.css'
 import AppRoute from '../Routes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights />
     <AppRoute />
   </StrictMode>,
 )
