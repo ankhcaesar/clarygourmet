cd05c5ddbeb1103d04207477c42b303ab1570651
ankhcaesar <ankhcaesar@gmail.com>
2025-07-27 10:11:20 -0300

    1

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import './styles/reset.css';
 import "./styles/variables.css";
-import "./styles/global.css"
+//import "./styles/global.css"
 import AppRoute from '../Routes'
 
 createRoot(document.getElementById('root')).render(
@@ -12,6 +12,5 @@ createRoot(document.getElementById('root')).render(
     <SpeedInsights />
 
     <AppRoute />
-   
   </StrictMode>,
 )
