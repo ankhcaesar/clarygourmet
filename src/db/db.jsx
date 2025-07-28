f2b68e25969ecb1faf229db7f3adc30b102c14e5
ankhcaesar <ankhcaesar@gmail.com>
2025-07-27 21:31:52 -0300

    disponible en arts

--- src/db/db.jsx
+++ src/db/db.jsx
@@ -1,9 +1,9 @@
 import Dexie from "dexie";
 const db = new Dexie('clarygptovta');
-db.version(9).stores(
+db.version(10).stores(
     {
-        categorias: 'id_cats, categoria, imagen_categoria',
-        sub_categorias: 'id_subcats, sub_categorias, categoria, imagen_subcategoria',
+        categorias: 'id_cats, categoria, imagen_categoria, disponible',
+        sub_categorias: 'id_subcats, sub_categorias, categoria, imagen_subcategoria, disponible',
         articulos: 'id_arts, id_subcats,id_cats, articulo, descripcion, imagen_articulo, valor_venta, estado, presentacion, disponible ',
         ventas: 'id_vta, fecha_hora, id_cli, id_carrito, total_venta, entrega,id_entrega',
         carrito: 'id_carrito, [id_vta+id_arts], id_arts, nombreArts, cant, valor_venta, valor_x_cant',
